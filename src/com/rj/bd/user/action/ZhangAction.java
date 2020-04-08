001579f04a5ba4d1c5110fbabe04cbca53f963af
wyhjack <909904682@qq.com>
2020-04-08 16:40:53 +0800

    小张：将新建的UserAction.java这个类提交到本地仓库，目的是为了演示讲解Git分分支

---
+++ src/com/rj/bd/user/action/ZhangAction.java
@@ -0,0 +1,16 @@
+package com.rj.bd.user.action;
+/**
+ * @desc   
+ * @author wyh
+ * @time   2020年4月8日 下午4:39:36
+ */
+public class ZhangAction {
+
+	
+public void add()
+{
+  System.out.println("求和。。。。");	
+}
+	
+	
+}
